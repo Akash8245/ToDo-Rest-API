75da3eac3eff1057659a1c8e29b68c4c70dedcb1
Akash <akash93433@gmail.com>
2024-05-19 15:33:05 +0530

    welcome page

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome user !")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ),
     path('api/',include('api.urls')),
 ]
